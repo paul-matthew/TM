6f7098f9137483051bb47710947a7778916abf5f
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-10-13 21:46:56 -0400

    Baseline 1.0

---
+++ js/main.js
@@ -0,0 +1,263 @@
+(function() {
+
+	"use strict";
+
+	AOS.init({
+		ease: 'slide',
+		once: true
+	});
+
+	var slider = function(){
+
+		var heroSlider = document.querySelectorAll('.hero-slider');
+
+		if ( heroSlider.length > 0 ) {
+			var heroSlider = tns({
+				container: '.hero-slider',
+				items: 1,
+				mode: 'carousel',
+				autoplay: true,
+			  animateIn: 'tns-fadeIn',
+		    animateOut: 'tns-fadeOut',
+				speed: 700,
+				nav: true,
+				controls: false,
+				autoplayButtonOutput: false,
+			});
+		}
+
+		var carouselCourses = document.querySelectorAll('.carousel-courses');
+		if ( carouselCourses.length > 0 ) {
+
+			var coursesSlider = tns({
+				container: '.carousel-courses',
+				items: 1,
+				mode: 'carousel',
+				autoplay: true,
+			  animateIn: 'tns-fadeIn',
+		    animateOut: 'tns-fadeOut',
+				speed: 700,
+				nav: true,
+				gutter: 20,
+				controls: false,
+				autoHeight: true,
+				autoplayButtonOutput: false,
+				responsive:{
+					0:{
+						items: 1,
+						gutter: 0
+					},
+					600:{
+						items: 2,
+						gutter: 20
+					},
+					1000:{
+						items: 3,
+						gutter: 20
+					}
+				}
+			});
+
+		}
+
+		var carouselSlider = document.querySelectorAll('.carousel-testimony');
+		if ( carouselSlider.length > 0 ) {
+
+			var testimonySlider = tns({
+				container: '.carousel-testimony',
+				items: 1,
+				mode: 'carousel',
+				autoplay: true,
+			  animateIn: 'tns-fadeIn',
+		    animateOut: 'tns-fadeOut',
+				speed: 700,
+				nav: true,
+				gutter: 20,
+				controls: false,
+				autoplayButtonOutput: false,
+				responsive:{
+					0:{
+						items: 1,
+						gutter: 0
+					},
+					600:{
+						items: 2,
+						gutter: 20
+					},
+					1000:{
+						items: 3,
+						gutter: 20
+					}
+				}
+			});
+
+		}
+
+	}
+	slider();
+	
+	//COUNTER
+	'use trict';
+		// How long you want the animation to take, in ms
+		const animationDuration = 2000;
+		// Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
+		const frameDuration = 1000 / 60;
+		// Use that to calculate how many frames we need to complete the animation
+		const totalFrames = Math.round( animationDuration / frameDuration );
+		// An ease-out function that slows the count as it progresses
+		const easeOutQuad = t => t * ( 2 - t );
+
+
+		const numberWithCommas = n => {
+			return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		}
+
+		// The animation function, which takes an Element
+		const animateCountUp = el => {
+			let frame = 0;
+			const countTo = parseInt( el.innerHTML, 10 );
+			// Start the animation running 60 times per second
+			const counter = setInterval( () => {
+			frame++;
+			// Calculate our progress as a value between 0 and 1
+			// Pass that value to our easing function to get our
+			// progress on a curve
+			const progress = easeOutQuad( frame / totalFrames );
+			// Use the progress value to calculate the current count
+			const currentCount = Math.round( countTo * progress );
+
+			// If the current count has changed, update the element
+			if ( parseInt( el.innerHTML, 10 ) !== currentCount ) {
+			el.innerHTML = numberWithCommas(currentCount);
+		}
+
+		// If we’ve reached our last frame, stop the animation
+		if ( frame === totalFrames ) {
+			clearInterval( counter );
+		}
+		}, frameDuration );
+		};
+
+		// Run the animation on all elements with a class of ‘countup’
+		const runAnimations = () => {
+			const countupEls = document.querySelectorAll( '.countup' );
+			countupEls.forEach( animateCountUp );
+		};
+
+
+
+
+		// In Viewed
+		var elements;
+		var windowHeight;
+
+		function init() {
+			elements = document.querySelectorAll('.section-counter');
+			windowHeight = window.innerHeight;
+		}
+
+		function checkPosition() {
+			var i;
+			for (i = 0; i < elements.length; i++) {
+				var element = elements[i];
+				var positionFromTop = elements[i].getBoundingClientRect().top;
+			if (positionFromTop - windowHeight <= 0) {
+			if( !element.classList.contains('viewed') ) {
+			element.classList.add('viewed');
+			runAnimations();
+			} else {
+			if ( element.classList.contains('viewed') ) {
+
+			}
+		}
+
+		}
+		}
+		}
+
+		window.addEventListener('scroll', checkPosition);
+		window.addEventListener('resize', init);
+
+		init();
+		checkPosition();
+
+
+	//GLIGHTBOX
+	const lightbox = GLightbox({
+	  touchNavigation: true,
+	  loop: true,
+	  autoplayVideos: true
+	});
+
+
+})()
+
+
+
+//Services Card pop-up
+
+// Function to open the modal
+function openModal(modalId) {
+	var modal = document.getElementById(modalId);
+	modal.style.display = "block";
+  }
+  
+  // Function to close the modal
+  function closeModal(modalId) {
+	var modal = document.getElementById(modalId);
+	modal.style.display = "none";
+  }
+  
+  // Close modal if the user clicks outside of it
+  window.onclick = function (event) {
+	var modals = document.querySelectorAll(".portfolio-modal");
+	for (var i = 0; i < modals.length; i++) {
+	  var modal = modals[i];
+	  if (event.target === modal) {
+		modal.style.display = "none";
+	  }
+	}
+  };
+
+
+    // Function to filter FAQ questions based on search keywords
+	function filterFAQs() {
+		const input = document.getElementById('faqSearch').value.toLowerCase();
+		const questions = document.querySelectorAll('h4');
+		
+		questions.forEach(question => {
+		  const text = question.textContent.toLowerCase();
+		  const answer = question.nextElementSibling.textContent.toLowerCase();
+		  
+		  if (text.includes(input) || answer.includes(input)) {
+			question.style.display = 'block';
+			question.nextElementSibling.style.display = 'block';
+		  } else {
+			question.style.display = 'none';
+			question.nextElementSibling.style.display = 'none';
+		  }
+		});
+	  }
+
+	  //LOADER SCREEN
+
+	  document.addEventListener('DOMContentLoaded', function () {
+		// Get references to the loading screen and the loader (logo)
+		const loadingScreen = document.querySelector('.loading-screen');
+		const loader = document.querySelector('.loader');
+	
+		// Create an animation that fades in the loader
+		setTimeout(function () {
+			loader.style.opacity = 1;
+		}, 00); // Adjust the delay to control when the loader appears
+	
+		// When the loader animation is complete (after 3 seconds), hide the loading screen
+		setTimeout(function () {
+			loadingScreen.style.opacity = 0;
+			setTimeout(function () {
+				loadingScreen.style.display = 'none';
+			}, 2000); // Adjust the delay to control when the loading screen disappears
+		}, 2000); // Adjust the duration to match your desired 3-second loading animation
+	});
+	
+	
---
+++ js/model.js
@@ -0,0 +1,89 @@
+import * as THREE from './three.module.js';
+import SceneInit from './SceneInit.js';
+import { OrbitControls } from "./OrbitControls.js";
+
+
+import { GLTFLoader } from './GLTFLoader.js';
+
+const ROTATION_SPEED = 0.003; // Adjust the value as needed
+let loadedModel = null;
+
+function init() {
+  let modelRotation = 12;
+  const test = new SceneInit('myThreeJsCanvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('myThreeJsCanvas'),
+    antialias: true,
+    alpha: true // Set alpha to true for a transparent background
+  });
+  test.initialize();
+
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load('../public/carnival/scene.gltf', (gltfScene) => {
+    loadedModel = gltfScene;
+
+    if (window.innerWidth > 768) {
+      gltfScene.scene.position.set(0, -0.5, 0); // For non-mobile screens
+    } else {
+      gltfScene.scene.position.set(0, 0, 0); // For mobile screens
+    }
+
+    test.scene.add(gltfScene.scene);
+
+    // Initialize AnimationMixer
+    const mixer = new THREE.AnimationMixer(gltfScene.scene);
+
+    // Load animations from the gltfScene
+    const animations = gltfScene.animations;
+    const actions = animations.map((animation) => mixer.clipAction(animation));
+
+    // Start playing the animations
+    actions.forEach((action) => action.play());
+
+    // Create an animate function for rendering
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      // Update the animation mixer
+      const delta = test.clock.getDelta();
+      mixer.update(delta);
+
+      // Rotate the model
+      modelRotation += ROTATION_SPEED;
+      if (loadedModel) {
+        gltfScene.scene.rotation.y = modelRotation;
+      }
+
+      renderer.render(test.scene, test.camera);
+    };
+
+    // Start the animation loop
+    animate();
+
+    // Add OrbitControls
+    const controls = new OrbitControls(test.camera, renderer.domElement);
+    controls.enableDamping = true; // Add damping for smooth camera movement
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false; // Set to false for more intuitive panning
+    controls.minDistance = 1;
+    controls.maxDistance = 100;
+
+// Increase the intensity of the ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 20.0); // Higher intensity
+test.scene.add(ambientLight);
+
+// Increase the intensity of the directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 20.0); // Higher intensity
+directionalLight.position.set(10, 100, 10); // Adjust position as needed
+test.scene.add(directionalLight);
+
+
+
+
+
+
+  });
+}
+
+// Initialize the Three.js scene
+init();
